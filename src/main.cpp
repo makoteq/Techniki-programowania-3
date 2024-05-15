c047312378a6d3da324e073b065483e279034267
Maciej Kotowski <makoteq@gmail.com>
2024-05-15 23:53:00 +0200

    function generate signal

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <pybind11/pybind11.h>
 #include <matplot/matplot.h>
+#include <cmath>
 #include "AudioFile.h"
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
-
+#define INTERVAL 10
+#define SPAN (10*INTERVAL)
 
 int add(int i, int j){
 
@@ -28,6 +30,69 @@ namespace plt = matplot;
     plt::show();
 }
 
+int generateSignal(int mode, int interval) {
+    std::vector<double> x;
+    std::vector<double> y;
+    int index = 0;
+    bool state = true;
+    switch (mode) {
+        case 0 :
+            for (float i = 0; i < SPAN; i++) {
+                x.push_back(i);
+                y.push_back(sin((i / (SPAN / 2 / interval)) * matplot::pi));
+            }
+            matplot::ylim({-1.5, +1.5});
+            matplot::plot(x, y, "-o");
+            break;
+        case 1 :
+            for (float i = 0; i < SPAN; i++) {
+                x.push_back(i);
+                y.push_back(cos((i / (SPAN / 2 / interval)) * matplot::pi));
+            }
+            matplot::ylim({-1.5, +1.5});
+            matplot::plot(x, y, "-o");
+            break;
+        case 2:
+            for (int i = 0; i < SPAN; i++) {
+                if (index == (SPAN / (interval * 2))) {
+                    state = !state;
+                    index = 0;
+                }
+                x.push_back(i);
+                y.push_back(state);
+                index++;
+            }
+            matplot::stairs(x, y, "-o");
+            matplot::ylim({-1, +2});
+            break;
+        case 3:
+            state = false;
+            x.push_back(0);
+            y.push_back(state);
+            state=true;
+            for (int i = 0; i < INTERVAL; i++) {
+                if(i) {
+                    x.push_back(i);
+                    y.push_back(state);
+                    state = false;
+                    x.push_back(i);
+                    y.push_back(state);
+                    state = true;
+                }
+
+            }
+            matplot::plot(x, y, "-o");
+            matplot::ylim({-1, +2});
+            break;
+
+    }
+
+    matplot::xlabel("number of samples in 1 second");
+    matplot::ylabel("amplitude");
+    matplot::show();
+    return 0;
+}
+
 namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
@@ -41,7 +106,7 @@ PYBIND11_MODULE(_core, m) {
            :toctree: _generate
 
            add
-           subtract
+           generateSignal
     )pbdoc";
 
     m.def("add", &add, R"pbdoc(
@@ -49,11 +114,10 @@ PYBIND11_MODULE(_core, m) {
 
         Some other explanation about the add function.
     )pbdoc");
+m.def("generateSignal", &generateSignal, R"pbdoc(
+        Add two numbers
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
+        Some other explanation about the add function.
     )pbdoc");
 
 #ifdef VERSION_INFO
