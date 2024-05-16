cabf00880233e0d089ef6637a3d4d7c001812142
Maciej Kotowski <makoteq@gmail.com>
2024-05-16 22:03:33 +0200

    add more tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import scikit_build_example as m
-
+m.differenceSignal(100,"./test-audio.wav")
 m.generateSignal(0,5)
 m.visualizeSignal(100,"./test-audio.wav")
 m.Discrete_Fourier_transform([1, 2, 0, -1], [0, -1, -1, 2])
