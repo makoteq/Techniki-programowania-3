467ea7ab2ba9e8e6644f4295d438447d43e9804c
Maciej Kotowski <makoteq@gmail.com>
2024-05-17 15:19:56 +0200

    add comments for tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,15 @@
 from __future__ import annotations
 
 import scikit_build_example as m
-m.visualizeSignal(100,"./test-audio.wav")
-m.differenceSignal(100,"./test-audio.wav")
-m.generateSignal(3,5)
+
+# span, path
+m.visualizeSignal(100, "./test-audio.wav")
+
+# span, path
+m.differenceSignal(100, "./test-audio.wav")
+
+# mode, frequency
+m.generateSignal(3, 5)
+
+# first array for real, second for imaginary
 m.Discrete_Fourier_transform([1, 2, 0, -1], [0, -1, -1, 2])
